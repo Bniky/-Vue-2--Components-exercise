e9728e7f9d78972b2d59f01db1cd3ce630421b77
Nicholas <nicholas.belo@yahoo.co.uk>
2021-07-06 19:10:57 +0100

    Files added for exercise

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # [Vue 2] Components exercise
  Passing data to child components, event bus, Validating passed data
+This is an exercise I've done going through Udemy course for Vue JS 2
---
+++ src/Announcement.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="alert alert-info">
+        <div v-if="announcement">
+            <p>{{ announcement }}</p>
+        </div>
+        <div v-else>
+            <p>No announcement yet.</p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import { eventBus } from './main';
+import Social from './Social.vue';
+
+    export default {
+        data() {
+            return {
+                announcement: null,        
+            };
+        },
+        components: {
+            appSocialShare: Social
+        },
+        created() {
+            eventBus.$on('articleWasShared', (data) => {
+                this.announcement = data.media
+                console.log(data);
+            });
+        }
+    }
+</script>
